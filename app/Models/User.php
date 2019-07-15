7f0b4c7ff9804a7a96e47fc51f2ae20bb2cd11e1
Liem Le <liemlv@nal.vn>
2019-07-15 15:49:26 +0700

    register and login

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Mopdels;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'address', 'birthday', 'last_login_ip', 'last_login_at'
     ];
 
     /**
